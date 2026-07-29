48dfd0f5ef3e42b3a0656023bc77068506f3a5f3
Jackson <jackson@homegolem.net>
2026-07-28 21:13:05 -0400

    kb rules update

--- dot_config/hypr/input.lua
+++ dot_config/hypr/input.lua
@@ -18,7 +18,7 @@ hl.config({
         kb_variant = {  },
         kb_model = {  },
         kb_options = {  },
-        kb_rules = {  },
+        kb_rules = "",
         follow_mouse = 1,
         sensitivity = 0,
         -- -1.0 - 1.0, 0 means no modification.
