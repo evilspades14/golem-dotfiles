af9b300003cddab0c3e3e424bc6c6285c4918378
Jackson <jackson@homegolem.net>
2026-09-09 14:46:20 -0400

    Update environment.lua

--- dot_config/hypr/environment.lua
+++ dot_config/hypr/environment.lua
@@ -12,9 +12,8 @@
 
 hl.env("XCURSOR_SIZE", 24)
 hl.env("HYPRCURSOR_SIZE", 24)
-hl.env("GTK_THEME", "Qogir-Dark")
-hl.env("QT_QPA_PLATFORMTHEME", "qt6ct")
-hl.env("QT_QPA_PLATFORM", "wayland")
+-- hl.env("QT_QPA_PLATFORMTHEME", "qt6ct")
+-- hl.env("QT_QPA_PLATFORM", "wayland")
 
 hl.config({
   xwayland = {
