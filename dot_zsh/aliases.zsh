9ed8c64e7c04e9cc70605bafb2f78d8fc928d183
Jackson <jackson@homegolem.net>
2026-09-19 23:15:54 -0400

    Delete aliases.zsh

--- dot_zsh/aliases.zsh
+++
@@ -1,9 +0,0 @@
-alias reboot-windows="systemctl reboot --boot-loader-entry='auto-windows'"
-alias ls="eza -l --icons -a"
-if [ "$TERM" = "xterm-kitty" ]; then
-    alias ssh='kitty +kitten ssh'
-fi
-
-alias caddy-reload="ssh evilspades@catapult-dragon.local 'docker exec caddy caddy -c /etc/caddy/Caddyfile reload'"
-alias caddy-edit="dockhand-edit container caddy /etc/caddy/Caddyfile --env catapult-dragon; caddy-reload"
-
